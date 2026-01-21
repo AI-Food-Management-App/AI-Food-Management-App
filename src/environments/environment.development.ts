89c7665863803ab51444626d8b29054f6f5d949d
GemmaRyan <157807096+GemmaRyan@users.noreply.github.com>
2026-01-21 11:26:43 +0000

    Commiting existing code

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: "/api", // dev uses proxy so still /api
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: "/api", // in production you can still use /api behind reverse proxy
+};
