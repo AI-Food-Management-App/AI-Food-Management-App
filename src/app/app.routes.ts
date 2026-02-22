7f092f9a325c6d1fdbdf38a73aec285ff9dce79c
Elain Polakova - STUDENT <S00250500@atu.ie>
2026-02-22 03:04:03 +0000

    Got most of the Home page UI done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,16 @@ import { Routes } from '@angular/router';
 import { ShoppingListComponent } from "./components/shopping-list/shopping-list.component";
 import { UploadComponent } from './components/upload/upload.component';
 import { SearchComponent } from './components/search/search.component';
+import { Home } from './components/home/home';
 
 // export const routes: Routes = [
 //      { path: "shopping-list", component: ShoppingListComponent },
 // ];
 
 export const routes = [
-  { path: "", component: UploadComponent },
+  { path: "", component: Home },
+  { path: "scan", component: UploadComponent }, // ML page
   { path: "shopping", component: ShoppingListComponent },
-  { path: "search", component: SearchComponent },
+  { path: "search", component: SearchComponent }
 ];
 
