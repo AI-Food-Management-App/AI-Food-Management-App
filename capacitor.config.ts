2f48b93fa3fcffb72f65f933b1c82ce7554fc080
GemmaRyan <157807096+GemmaRyan@users.noreply.github.com>
2026-03-06 23:53:38 +0000

    Added capacitor running on android studio using npx cap open android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gemma.foodapp',
+  appName: 'food-management-app-frontend',
+  webDir: 'dist/FoodManagementApp-frontend/browser'
+};
+
+export default config;
