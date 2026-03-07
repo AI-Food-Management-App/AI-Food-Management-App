9dff923fa856a86168b023a28a3633b957b2f762
Elain Polakova - STUDENT <S00250500@atu.ie>
2026-03-07 02:46:12 +0000

    adding in inventory tab to seperate it from the shopping tab

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { UploadComponent } from './components/upload/upload.component';
 import { SearchComponent } from './components/search/search.component';
 import { ProfileComponent } from './components/profile/profile';
 import { authGuard } from './guards/auth.guard';
+import { InventoryComponent } from './components/inventory/inventory.component';
 
 export const routes: Routes = [
   { path: '',         component: Home },
@@ -16,6 +17,7 @@ export const routes: Routes = [
   { path: 'shopping', component: ShoppingListComponent, canActivate: [authGuard] },
   { path: 'search',   component: SearchComponent, canActivate: [authGuard] },
   { path: 'profile',  component: ProfileComponent, canActivate: [authGuard] },
+  { path: 'inventory', component: InventoryComponent, canActivate: [authGuard] },
 
   // Catch-all — redirect unknown paths to home
   { path: '**', redirectTo: '' }
