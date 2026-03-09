372878787018a951d54cfcb180c1160caf27bd1d
GemmaRyan <s00250864@atu.ie>
2026-03-09 14:11:13 +0000

    commtting eith no guard rails

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,11 +13,17 @@ export const routes: Routes = [
   { path: '',         component: Home },
   { path: 'login',    component: LoginComponent },     
   { path: 'register', component: RegisterComponent },   
-  { path: 'scan',     component: UploadComponent, canActivate: [authGuard] },
-  { path: 'shopping', component: ShoppingListComponent, canActivate: [authGuard] },
-  { path: 'search',   component: SearchComponent, canActivate: [authGuard] },
-  { path: 'profile',  component: ProfileComponent, canActivate: [authGuard] },
-  { path: 'inventory', component: InventoryComponent, canActivate: [authGuard] },
+  // { path: 'scan',     component: UploadComponent, canActivate: [authGuard] },
+  // { path: 'shopping', component: ShoppingListComponent, canActivate: [authGuard] },
+  // { path: 'search',   component: SearchComponent, canActivate: [authGuard] },
+  // { path: 'profile',  component: ProfileComponent, canActivate: [authGuard] },
+  // { path: 'inventory', component: InventoryComponent, canActivate: [authGuard] },
+
+   { path: 'scan',     component: UploadComponent },
+  { path: 'shopping', component: ShoppingListComponent },
+  { path: 'search',   component: SearchComponent },
+  { path: 'profile',  component: ProfileComponent },
+  { path: 'inventory', component: InventoryComponent},
 
   // Catch-all — redirect unknown paths to home
   { path: '**', redirectTo: '' }
